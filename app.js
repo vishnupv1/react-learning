8cb05404363f2f7a713952ed78eb575e56443f7e
Vishnu P V <vishnupv0808@gmail.com>
2024-01-25 14:53:02 +0530

    React importing for the first time

---
+++ app.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+const heading = React.createElement("h1", {}, "React is here");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+// heading.innerHTML = "Name : Vishnu P V";
+// const root = document.getElementById("root");
+// root.appendChild(heading);
---
+++ dist/index.7c0ccee6.js
@@ -0,0 +1,8 @@
+impo;
+const heading = React.createElement("h1", {}, "React is here");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading); // heading.innerHTML = "Name : Vishnu P V";
+ // const root = document.getElementById("root");
+ // root.appendChild(heading);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
