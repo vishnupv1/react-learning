bedf01ff76985fcaa091b15d579a348869087f09
Vishnu P V <vishnupv0808@gmail.com>
2024-01-25 15:43:19 +0530

    parcel in depth

--- dist/index.7c0ccee6.js
+++
@@ -1,8 +0,0 @@
-impo;
-const heading = React.createElement("h1", {}, "React is here");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading); // heading.innerHTML = "Name : Vishnu P V";
- // const root = document.getElementById("root");
- // root.appendChild(heading);
-
-//# sourceMappingURL=index.7c0ccee6.js.map
