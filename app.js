baebfd7e0d9fd970378bd3cafbdc30cccfd4b0f7
Vishnu P V <vishnupv0808@gmail.com>
2024-01-25 15:21:52 +0530

    Experiencing HMR (Hot module reloading)

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {}, "React is here");
+const heading = React.createElement("h1", {}, "React is here at browser");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 // heading.innerHTML = "Name : Vishnu P V";
